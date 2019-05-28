2335de684805cebad336244671431ed9fbef6aa7
Ben Shealy <bentshermann@gmail.com>
2019-05-28 10:20:41 -0400

    Refactored kube-run.sh to submit a job with 1 pod

--- kube-run.sh
+++ kube-run.sh
@@ -12,6 +12,9 @@ PVC_NAME="$2"
 POD_NAME="$3"
 SPEC_FILE="pod.yaml"
 
+PVC_PATH="/workspace"
+CFG_PATH="/etc/nextflow"
+
 # write pod spec to file
 cat > $SPEC_FILE <<EOF
 apiVersion: v1
@@ -19,46 +22,51 @@ kind: ConfigMap
 metadata:
   name: $POD_NAME
 data:
-  init.sh: "mkdir -p ''/workspace/$USER''; if [ -d ''/workspace/$USER'' ]; then cd ''/workspace/$USER''; else echo ''Cannot create directory: /workspace/$USER''; exit 1; fi; [ -f /etc/nextflow/scm ] && ln -s /etc/nextflow/scm $NXF_HOME/scm; [ -f /etc/nextflow/nextflow.config ] && cp /etc/nextflow/nextflow.config $PWD/nextflow.config; "
+  init.sh: "mkdir -p ''$PVC_PATH/$USER''; if [ -d ''$PVC_PATH/$USER'' ]; then cd ''$PVC_PATH/$USER''; else echo ''Cannot create directory: $PVC_PATH/$USER''; exit 1; fi; [ -f $CFG_PATH/scm ] && ln -s $CFG_PATH/scm $NXF_HOME/scm; [ -f $CFG_PATH/nextflow.config ] && cp $CFG_PATH/nextflow.config $PWD/nextflow.config; "
 ---
-apiVersion: v1
-kind: Pod
+apiVersion: batch/v1
+kind: Job
 metadata:
   name: $POD_NAME
 spec:
-  containers:
-  - name: $POD_NAME
-    image: nextflow/nextflow:19.04.0
-    imagePullPolicy: IfNotPresent
-    env:
-    - name: NXF_WORK
-      value: /workspace/$USER/work
-    - name: NXF_ASSETS
-      value: /workspace/projects
-    - name: NXF_EXECUTOR
-      value: k8s
-    command:
-    - /bin/bash
-    - -c
-    - source /etc/nextflow/init.sh; nextflow run $PIPELINE
-    resources:
-      limits:
-        memory: 4Gi
-      requests:
-        memory: 256Mi
-    volumeMounts:
-    - name: vol-1
-      mountPath: /workspace
-    - name: vol-2
-      mountPath: /etc/nextflow
-  restartPolicy: Never
-  volumes:
-  - name: vol-1
-    persistentVolumeClaim:
-      claimName: $PVC_NAME
-  - name: vol-2
-    configMap:
-      name: $POD_NAME
+  completions: 1
+  template:
+    spec:
+      containers:
+      - name: $POD_NAME
+        image: nextflow/nextflow:19.04.0
+        imagePullPolicy: IfNotPresent
+        env:
+        - name: NXF_WORK
+          value: $PVC_PATH/$USER/work
+        - name: NXF_ASSETS
+          value: $PVC_PATH/projects
+        - name: NXF_EXECUTOR
+          value: k8s
+        command:
+        - /bin/bash
+        - -c
+        - source $CFG_PATH/init.sh; nextflow run $PIPELINE
+        resources:
+          limits:
+            cpu: 1
+            memory: 4Gi
+          requests:
+            cpu: 1
+            memory: 256Mi
+        volumeMounts:
+        - name: vol-1
+          mountPath: $PVC_PATH
+        - name: vol-2
+          mountPath: $CFG_PATH
+      restartPolicy: Never
+      volumes:
+      - name: vol-1
+        persistentVolumeClaim:
+          claimName: $PVC_NAME
+      - name: vol-2
+        configMap:
+          name: $POD_NAME
 EOF
 
 # create pod
