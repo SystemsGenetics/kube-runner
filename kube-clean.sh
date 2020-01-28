1fbe20f72b1b65177ec250233c11c819be100e02
Ben Shealy <bentshermann@gmail.com>
2020-01-28 12:29:38 -0500

    Updated kube-clean.sh to also delete ContainerCannotRun pods

--- kube-clean.sh
+++ kube-clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Remove all pods that are not running.
 
-PODS=$(kubectl get pods --no-headers | grep 'Error\|Completed' | awk '{ print $1 }')
+PODS=$(kubectl get pods --no-headers | grep 'Completed\|Error\|ContainerCannotRun' | awk '{ print $1 }')
 
 if [[ ! -z $PODS ]]; then
 	kubectl delete pods $PODS
